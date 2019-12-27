251886847c82eaea1c4e09670b80a46b9cb5f57a
sanjay kumar <sanjay@yahoo.com>
2019-12-27 23:23:10 +0530

    this is the starter template

---
+++ VideoTube frontend/assets/js/commonActions.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$(".nav-show-hide").on("click", function(){
+		
+		var main = $("#main-section-container");
+		var nav = $("#side-nav-container");
+
+		if (main.hasClass("leftPadding")) {
+			nav.hide();
+
+		}else{
+			nav.show();
+
+		}
+
+		main.toggleClass("leftPadding");
+
+	});
+});
